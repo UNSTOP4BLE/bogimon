e60bb26e2288945e2e27f1e67be0b348ec9127f0
UNSTOP4BLE <bruno.juraski1@gmail.com>
2022-02-02 21:15:13 +0100

    Initial commit

---
+++ src/stage/week1.c
@@ -0,0 +1,71 @@
+/*
+  This Source Code Form is subject to the terms of the Mozilla Public
+  License, v. 2.0. If a copy of the MPL was not distributed with this
+  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#include "week1.h"
+
+#include "../archive.h"
+#include "../mem.h"
+#include "../stage.h"
+
+//Week 1 background structure
+typedef struct
+{
+	//Stage background base structure
+	StageBack back;
+	
+	//Textures
+	Gfx_Tex tex_back0; //bg
+} Back_Week1;
+
+//Week 1 background functions
+void Back_Week1_DrawBG(StageBack *back)
+{
+	Back_Week1 *this = (Back_Week1*)back;
+	
+	fixed_t fx, fy;
+
+	//Draw back
+	fx = stage.camera.x;
+	fy = stage.camera.y;
+
+	RECT back_src = {0, 0, 256, 256};
+	RECT_FIXED back_dst = {
+		FIXED_DEC(-190,1) - fx,
+		FIXED_DEC(-100,1) - fy,
+		FIXED_DEC(121,1),
+		FIXED_DEC(105,1)
+	};
+	Stage_DrawTex(&this->tex_back0, &back_src, &back_dst, stage.camera.bzoom);
+}
+
+void Back_Week1_Free(StageBack *back)
+{
+	Back_Week1 *this = (Back_Week1*)back;
+	
+	//Free structure
+	Mem_Free(this);
+}
+
+StageBack *Back_Week1_New(void)
+{
+	//Allocate background structure
+	Back_Week1 *this = (Back_Week1*)Mem_Alloc(sizeof(Back_Week1));
+	if (this == NULL)
+		return NULL;
+	
+	//Set background functions
+	this->back.draw_fg = NULL;
+	this->back.draw_md = NULL;
+	this->back.draw_bg = Back_Week1_DrawBG;
+	this->back.free = Back_Week1_Free;
+	
+	//Load background textures
+	IO_Data arc_back = IO_Read("\\WEEK1\\BACK.ARC;1");
+	Gfx_LoadTex(&this->tex_back0, Archive_Find(arc_back, "back0.tim"), 0);
+	Mem_Free(arc_back);
+	
+	return (StageBack*)this;
+}
---
+++ src/stagedef_disc1.h
@@ -0,0 +1,16 @@
+	{ //StageId_1_1 (Bopeebo)
+		//Characters
+		{Char_BF_New,    FIXED_DEC(60,1),  FIXED_DEC(100,1)},
+		{Char_Dad_New, FIXED_DEC(-120,1),  FIXED_DEC(100,1)},
+		{NULL},
+
+		//Stage background
+		Back_Week1_New,
+		
+		//Song info
+		{FIXED_DEC(3,1),FIXED_DEC(3,1),FIXED_DEC(3,1)},
+		1, 1,
+		XA_Bopeebo, 0,
+		
+		StageId_1_1, 0
+	},
